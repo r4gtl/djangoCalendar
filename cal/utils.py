488c3db4bc76792a56799d961514787b66435a06
r4gtl <rquattrogtl@gmail.com>
2023-02-24 17:15:06 +0100

    Adding week

--- cal/utils.py
+++ cal/utils.py
@@ -157,8 +157,12 @@ class Calendar(HTMLCalendar):
 		cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
 		cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
 		cal += f'{self.formatweekheader()}\n'
+		count=0
 		for theweek in self.monthdays2calendar(self.year, self.month):
-			cal += f'{self.formatweekOnly(theweek, week, events)}\n' 		
+			if count==week-1:
+				cal += f'{self.formatweek(theweek, events)}\n' 		
+			count+=1
+
   		# count=0
   		
 		# for week in self.monthdays2calendar(self.year, self.month):
